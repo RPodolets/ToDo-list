dbcfff4b5523e464f81be6f29ac7523a96d31f25
Roman Podolets <romanpyu@gmail.com>
2023-10-05 10:41:41 +0300

    Add models: Tag, Task

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.datetime_safe import datetime
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=511)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True)
+    is_done = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self) -> str:
+        return self.content
+
+    def is_end_date(self):
+        return datetime.now() > self.deadline
